b9a61c1e25aad0654d3c9cedfa30847dbd3b6665
donggeugkim <kdgmail2000@hanmail.net>
2022-05-18 17:29:06 +0900

    [섹션 7-5] ViewPager - 페이지 위치 조정

--- app/src/main/java/com/shoppi/app/HomeFragment.kt
+++ app/src/main/java/com/shoppi/app/HomeFragment.kt
@@ -10,6 +10,7 @@ import androidx.fragment.app.Fragment
 import org.json.JSONObject
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.google.gson.Gson
 
 class HomeFragment : Fragment() {
@@ -28,7 +29,7 @@ class HomeFragment : Fragment() {
         val toolbarTitle = view.findViewById<TextView>(R.id.toolbar_home_title)
         val toolbarIcon = view.findViewById<ImageView>(R.id.toolbar_home_icon)
         val viewpager = view.findViewById<ViewPager2>(R.id.viewpager_home_banner)
-        //val viewpagerIndicator = view.findViewById<TabLayout>(R.id.viewpager_home_banner_indicator)
+        val viewpagerIndicator = view.findViewById<TabLayout>(R.id.viewpager_home_banner_indicator)
 
         val assetLoader = AssetLoader()
         val homeData = assetLoader.getJsonString(requireContext(), "home.json")
@@ -55,6 +56,21 @@ class HomeFragment : Fragment() {
                 viewpager.adapter = HomeBannerAdapter().apply {
                     submitList(homeData.topBanners)
                 }
+                val pageWidth = resources.getDimension(R.dimen.viewpager_item_width)
+                val pageMargin = resources.getDimension(R.dimen.viewpager_item_margin)
+                val screenWidth = resources.displayMetrics.widthPixels
+                val offset  = screenWidth - pageWidth - pageMargin
+
+                viewpager.offscreenPageLimit = 3
+                viewpager.setPageTransformer { page, position ->
+                    page.translationX = position * -offset
+                }
+
+                TabLayoutMediator(viewpagerIndicator, viewpager) { tab, position ->
+
+                }.attach()
+
+
             }
         }
     }
