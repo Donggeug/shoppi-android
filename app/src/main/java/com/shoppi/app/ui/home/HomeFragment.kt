171b008ce87e7ab96ae914d8bee69b1725f360ce
donggeugkim <kdgmail2000@hanmail.net>
2022-06-22 22:50:54 +0900

    [섹션 12] 과제 : 장바구니 화면 Layout 구현

--- app/src/main/java/com/shoppi/app/ui/home/HomeFragment.kt
+++ app/src/main/java/com/shoppi/app/ui/home/HomeFragment.kt
@@ -8,14 +8,18 @@ import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ConcatAdapter
 import com.google.android.material.tabs.TabLayoutMediator
 import com.shoppi.app.*
 import com.shoppi.app.common.KEY_PRODUCT_ID
 import com.shoppi.app.databinding.FragmentHomeBinding
+import com.shoppi.app.ui.common.ProductPromotionAdapter
+import com.shoppi.app.ui.common.SectionTitleAdapter
 import com.shoppi.app.ui.common.EventObserver
+import com.shoppi.app.ui.common.ProductClickListener
 import com.shoppi.app.ui.common.ViewModelFactory
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), ProductClickListener {
 
     private val viewModel: HomeViewModel by viewModels { ViewModelFactory(requireContext()) }
     private lateinit var binding: FragmentHomeBinding
@@ -36,6 +40,14 @@ class HomeFragment : Fragment() {
         setToolbar()
         setTopBanners()
         setNavigation()
+        setListAdapter()
+    }
+
+    // ProductClickListener
+    override fun onProductClick(productId: String) {
+        findNavController().navigate(R.id.action_home_to_product_detail, bundleOf(
+            KEY_PRODUCT_ID to "desk-1"
+        ))
     }
 
     private fun setToolbar() {
@@ -78,5 +90,14 @@ class HomeFragment : Fragment() {
         }
     }
 
+    private fun setListAdapter() {
+        val titleAdapter = SectionTitleAdapter()
+        val promotionAdapter = ProductPromotionAdapter(this)
+        binding.rvHome.adapter = ConcatAdapter(titleAdapter, promotionAdapter)
+        viewModel.promotions.observe(viewLifecycleOwner) { promotions ->
+            titleAdapter.submitList(listOf(promotions.title))
+            promotionAdapter.submitList(promotions.items)
+        }
+    }
 
 }
